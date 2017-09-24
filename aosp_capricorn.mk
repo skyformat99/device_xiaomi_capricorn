9d636855cd860d972dd765b1a0b618cd9430b9a8
Egor Ermakov <typoumdk@gmail.com>
2017-09-24 12:02:28 +0500

    fix

--- aosp_capricorn.mk
+++ aosp_capricorn.mk
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from gemini device
-$(call inherit-product, device/xiaomi/gemini/device.mk)
+# Inherit from capricorn device
+$(call inherit-product, device/xiaomi/capricorn/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/aosp/common.mk)
